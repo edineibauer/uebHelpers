b56df374e24927ef7469efbc42bfb02b7817f562
Edinei J. Bauer <galera.org@gmail.com>
2020-03-11 00:00:50 -0300

    cepaberto and cepGeoCode add to Helpers

--- public/src/Helpers/Helper.php
+++ public/src/Helpers/Helper.php
@@ -142,6 +142,8 @@ class Helper
     }
 
     /**
+     * Busca CEP no Cep Aberto e retorna dados
+     *
      * @param string $cep
      * @return string
      */
@@ -153,9 +155,31 @@ class Helper
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Token token="' . CEPABERTO . '"'));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            return curl_exec($ch);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,0);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+            return json_decode(curl_exec($ch), !0);
         }
         return 'token CEP ABERTO não definido';
+    }
+
+    /**
+     * Busca CEP no google Geo Code e retorna dados
+     * @param string $cep
+     * @return bool|string
+     */
+    public static function cepGeoCode(string $cep)
+    {
+        if(defined("GEOCODE") && !empty(GEOCODE)) {
+            $url = "https://maps.googleapis.com/maps/api/geocode/json?address=" . $cep . "&key=" . GEOCODE;
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+            $result = json_decode(curl_exec($ch), !0);
+            return ($result['status'] === "OK" ? $result['results'] : []);
+        }
+        return 'token GEO CODE não definido';
     }
 
     /**

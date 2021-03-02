303b27d44452037f31848206c8c1daa9846ccf5e
edineijacobbauer <edineibauer@gmail.com>
2021-03-02 01:54:03 -0300

    fiz cpf and cnpj validator syntax

--- public/src/Helpers/Check.php
+++ public/src/Helpers/Check.php
@@ -139,20 +139,20 @@ class Check
 
         // Valida primeiro dígito verificador
         for ($i = 0, $j = 5, $soma = 0; $i < 12; $i++) {
-            $soma += $cnpj{$i} * $j;
+            $soma += $cnpj[$i] * $j;
             $j = ($j == 2) ? 9 : $j - 1;
         }
         $resto = $soma % 11;
-        if ($cnpj{12} != ($resto < 2 ? 0 : 11 - $resto))
+        if ($cnpj[12] != ($resto < 2 ? 0 : 11 - $resto))
             return false;
 
         // Valida segundo dígito verificador
         for ($i = 0, $j = 6, $soma = 0; $i < 13; $i++) {
-            $soma += $cnpj{$i} * $j;
+            $soma += $cnpj[$i] * $j;
             $j = ($j == 2) ? 9 : $j - 1;
         }
         $resto = $soma % 11;
-        return $cnpj{13} == ($resto < 2 ? 0 : 11 - $resto);
+        return $cnpj[13] == ($resto < 2 ? 0 : 11 - $resto);
     }
 
     /**
@@ -169,21 +169,20 @@ class Check
         endif;
 
         for ($i = 0, $j = 10, $soma = 0; $i < 9; $i++, $j--):
-            $soma += $cpf{$i} * $j;
+            $soma += $cpf[$i] * $j;
         endfor;
 
         $resto = $soma % 11;
-        if ($cpf{9} != ($resto < 2 ? 0 : 11 - $resto)):
+        if ($cpf[9] != ($resto < 2 ? 0 : 11 - $resto)):
             return false;
         endif;
 
         for ($i = 0, $j = 11, $soma = 0; $i < 10; $i++, $j--):
-            $soma += $cpf{$i} * $j;
+            $soma += $cpf[$i] * $j;
         endfor;
 
         $resto = $soma % 11;
-        return $cpf{10} == ($resto < 2 ? 0 : 11 - $resto);
-
+        return $cpf[10] == ($resto < 2 ? 0 : 11 - $resto);
     }
 
     /**
